f104fb9b52a160bd8e8844b53da4787ce7352177
chakroun-elias <eliaschakroun.01@gmail.com>
2023-01-18 14:05:01 +0100

    update script.js

--- script.js
+++ script.js
@@ -5,52 +5,97 @@ const deleteNum = document.querySelector('.delete');
 const equalSign = document.querySelector('.equal');
 const output = document.getElementById('output');
 let outputValue = '';
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let result = '';
+
+output.textContent = '0';
+
+allClear.addEventListener('click', () => {
+        firstNumber = '';
+        secondNumber = '';
+        operator = '';
+        output.textContent = '0';
+});
+
+deleteNum.addEventListener('click', () => {
+    if (operator === '') {
+        firstNumber = firstNumber.substring(1);
+    /* } else if (operator === '+' || operator === '-' || operator === '*' || operator === '÷') {
+        operator = operator.substring(1); */
+    } else {
+        secondNumber = secondNumber.substring(1);
+    }
+    output.textContent = firstNumber + operator + secondNumber;
+})
 
 numbers.forEach((button) => {
     button.addEventListener('click', (event) => {
         const buttonValue = event.target.textContent;
-        if (outputValue === '0') {
-            outputValue = buttonValue;
-        } else if (outputValue.length < 5) {
-        outputValue += buttonValue;
-        output.textContent = outputValue;
-}})
-})
+        if (operator === '') {
+            firstNumber += buttonValue;
+            output.textContent = firstNumber;
+        } else {
+            secondNumber += buttonValue;
+            output.textContent = firstNumber + operator + secondNumber;
+        }
+    });
+});
+
+operators.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        operator = event.target.textContent;
+        output.textContent = operator;
+    });
+});
+
+equalSign.addEventListener('click', () => {
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    operate(operator, firstNumber, secondNumber);
+    firstNumber = result;
+    secondNumber = '';
+    operator = '';
+});
 
 
 
 
 
-function add(x,y) {
-    return x + y;
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
 }
-function substract(x,y) {
-    return x - y;
+function substract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
 }
-function multiply(x,y) {
-    return x * y;
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
 }
-function divide(x,y) {
-    return x / y;
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
 }
 
-function operate(operator, x, y) {
+function operate(operator,firstNumber, secondNumber) {
+    let result;
     switch (operator) {
         case '+' :
-            return add(x,y);
+           result = add(firstNumber, secondNumber);
             break;
         case '-' :
-            return substract(x,y);
+           result = substract(firstNumber, secondNumber);
             break;
         case '*' :
-            return multiply(x,y);
+           result = multiply(firstNumber, secondNumber);
             break;
-        case '/':
-            return divide(x,y);
+        case '÷':
+           result = divide(firstNumber, secondNumber);
             break;
         default:
-            return 'Invalid operator';
+           result = 'Invalid operator';
         }
+        output.textContent = result;
 }
 
 
