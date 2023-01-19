90fa76e1c97a79fe485f38768e70a77bac7d1a07
chakroun-elias <eliaschakroun.01@gmail.com>
2023-01-19 11:47:10 +0100

    update script.js

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let firstNumber = '';
 let secondNumber = '';
 let operator = '';
 let intermediateResult = '';
+let operatorPressed = false;
+let previousResult1 = '';
+let previousResult2 = '';
 
 output.textContent = '0';
 
@@ -15,48 +18,75 @@ allClear.addEventListener('click', () => {
         firstNumber = '';
         secondNumber = '';
         operator = '';
+        operatorPressed = false;
+        intermediateResult = '';
         output.textContent = '0';
 });
 
 deleteNum.addEventListener('click', () => {
-    if (secondNumber !== '') {
+    if (secondNumber !== '' && secondNumber.length > 0) {
         secondNumber = secondNumber.substring(0, secondNumber.length - 1);
     } else if (operator !=='') {
         operator = operator.substring(0, operator.length - 1);
     }
-    else {
+    else if (firstNumber !== '' && firstNumber.length > 0) {
         firstNumber = firstNumber.substring(0, firstNumber.length - 1);
-    }
+    };
     output.textContent = firstNumber + operator + secondNumber;
-})
+});
+
 
 numbers.forEach((button) => {
     button.addEventListener('click', (event) => {
         const buttonValue = event.target.textContent;
-       if (firstNumber === '') {
-        firstNumber += buttonValue;
-        output.textContent = firstNumber;
-       } else {
-        secondNumber += buttonValue;
-        output.textContent = firstNumber + operator + secondNumber;
-       }
+        if (firstNumber === '') {
+            if (firstNumber.length < 5) {
+                firstNumber += buttonValue;
+                output.textContent = firstNumber;
+            }
+        } else {
+            if (operatorPressed) {
+                if (secondNumber.length < 5) {
+                    secondNumber += buttonValue;
+                    output.textContent = firstNumber + operator + secondNumber;
+                }
+            } else {
+                if (firstNumber.length < 5) {
+                    firstNumber += buttonValue;
+                    output.textContent = firstNumber;
+                }
+            }
+        }
     });
 });
 
+
+
 operators.forEach((button) => {
     button.addEventListener('click', (event) => {
-        operator = event.target.textContent;
-        output.textContent = firstNumber + operator + (secondNumber ? secondNumber : "");
+        if(parseFloat(firstNumber) !== '' && !operatorPressed) {
+            operator = event.target.textContent;
+            operatorPressed = true;
+            output.textContent = firstNumber + operator;
+        }
     });
 });
+
+
 
 equalSign.addEventListener('click', () => {
     if (firstNumber !== '' && secondNumber !== '' && operator !== '') {
         evaluateSinglePair();
         output.textContent = intermediateResult;
-        firstNumber = intermediateResult.toString();
+        firstNumber = '';
         secondNumber = '';
         operator = '';
+        operatorPressed = false;
+        if (previousResult1 === '') {
+            previousResult1 = intermediateResult;
+        } else {
+            previousResult2 = intermediateResult;
+        }
     } else {
         output.textContent = 'Invalid input';
     }
@@ -64,22 +94,22 @@ equalSign.addEventListener('click', () => {
 });
 
 
-function evaluateSinglePair() {
+/* function evaluateSinglePair() {
     if (firstNumber !== '' && secondNumber !== '' && operator !== '') {
-        firstNumber = parseFloat(firstNumber.toFixed(2));
-        secondNumber = parseFloat(secondNumber.toFixed(2));
+        firstNumber = parseFloat(firstNumber);
+        secondNumber = parseFloat(secondNumber);
         switch (operator) {
             case '+':
-                intermediateResult = firstNumber + secondNumber;
+                intermediateResult = peeviousResult + firstNumber + secondNumber;
                 break;
             case '-':
-                intermediateResult = firstNumber - secondNumber;
+                intermediateResult = peeviousResult - firstNumber - secondNumber;
                 break;
             case '*':
-                intermediateResult = firstNumber * secondNumber;
+                intermediateResult = peeviousResult * firstNumber * secondNumber;
                 break;
             case '÷':
-                intermediateResult = firstNumber / secondNumber;
+                intermediateResult = peeviousResult / firstNumber / secondNumber;
                 break;
             default:
                 intermediateResult = 'Invalid operator';
@@ -87,15 +117,26 @@ function evaluateSinglePair() {
     } else {
        intermediateResult = 'Invalid input';
     }
+} */
+function evaluateSinglePair() {
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    switch (operator) {
+        case '+':
+            intermediateResult = firstNumber + secondNumber;
+            break;
+        case '-':
+            intermediateResult = firstNumber - secondNumber;
+            break;
+        case '*':
+            intermediateResult = firstNumber * secondNumber;
+            break;
+        case '÷':
+            intermediateResult = firstNumber / secondNumber;
+            break;
+        default:
+            intermediateResult = 'Invalid operator';
+    }
+    firstNumber = intermediateResult;
+    secondNumber = '';
 }
- 
-
-
-
-
-
-
-
-
-
-
